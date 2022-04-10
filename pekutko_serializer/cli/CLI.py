63426b3b198a1e38fdde2ef01dc59eab526ffa25
DimaPekutko <dkefir03@gmail.com>
2022-04-10 10:54:13 +0300

    added config parsing

--- pekutko_serializer/cli/CLI.py
+++ pekutko_serializer/cli/CLI.py
@@ -1,8 +1,7 @@
 import argparse
 import os
 import sys
-
-from numpy import source
+import configparser
 
 from pekutko_serializer.serializer import create_serializer
 
@@ -25,22 +24,35 @@ class CLI:
         return self.__parser.parse_args()
 
     def exec_convert(self, cli_args):
+        source_path, to_path = None, None
+        # config strategy
         if cli_args.conf:
-            pass
+            source_path, to_path = self.__read_config(cli_args.conf)
+        # cli args strategy
         else:
             source_path, to_path = cli_args.source, cli_args.to
-            if source_path and to_path:
-                if os.path.exists(source_path) and os.path.isfile(source_path):
-                    cwd = os.getcwd()
-                    # run convert with absolute paths
-                    abs_source_path = os.path.join(cwd,str(source_path).strip())
-                    abs_to_path = os.path.join(cwd,str(to_path).strip())
-                    self.__convert(abs_source_path, abs_to_path)
-                else:
-                    self.__parser.error(f'No such file: "{source_path}"')
+        # execute convertation
+        if source_path and to_path:
+            if os.path.exists(source_path) and os.path.isfile(source_path):
+                abs_source_path = os.path.abspath(source_path)
+                abs_to_path = os.path.abspath(to_path)
+                self.__convert(abs_source_path, abs_to_path)
             else:
-                self.__parser.error(
-                    f'Invalid --source and --to flags: source="{source_path}", to="{to_path}"')
+                self.__parser.error(f'No such file: "{source_path}"')
+        else:
+            self.__parser.error(
+                f'Invalid --source and --to flags: source="{source_path}", to="{to_path}"')
+
+    def __read_config(self, config_path: str) -> tuple:
+        config_path = os.path.abspath(config_path)
+        if os.path.exists(config_path) and os.path.isfile(config_path):
+            config = configparser.RawConfigParser()
+            config.read(config_path)
+            source_path = config.get("DEFAULT", "source_path")
+            to_path = config.get("DEFAULT", "to_path")
+            return source_path.replace('"',''), to_path.replace('"','')
+        else:
+            self.__parser.error(f'Invalid config file path: "{config_path}"')
 
     def __convert(self, source_file_path: str, to_file_path: str):
         source_format = os.path.splitext(source_file_path)[1][1:]
